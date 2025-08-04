e3a1c2c7e33a8f5b492974c9a2f048ece3489320
NadmanAlvee <nadmanalvee@gmail.com>
2025-08-04 14:14:18 +0600

    fix: Update CORS origin and axios base URL for production; adjust HomePage layout for responsiveness

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -5,7 +5,9 @@ import { useChatStore } from "./useChatStore";
 import toast from "react-hot-toast";
 
 const BASE_URL =
-	import.meta.env.MODE === "development" ? "http://localhost:5001/" : "/";
+	import.meta.env.MODE === "development"
+		? "http://localhost:5001"
+		: "http://103.152.106.133:5001";
 
 export const useAuthStore = create((set, get) => ({
 	authUser: null,
